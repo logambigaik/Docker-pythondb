23b52ac15f6ee4511156baac7fefc283e58b1206
logambigaik <54719289+logambigaik@users.noreply.github.com>
2020-12-25 20:51:28 +0530

    Update application.py

--- application.py
+++ application.py
@@ -9,14 +9,18 @@ mysql=pymysql.connect(db='flaskapp', user='root', passwd='root', host='localhost
 @app.route('/',methods=['GET','POST'])
 
 def index():
+    cur=mysql.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Userdetail(firstname VARCHAR(20),lastname VARCHAR(20))")	
+    mysql.commit()
+ 			
     if request.method == "POST":
         details = request.form
         firstName = details['fname']
         lastName = details['lname']
-        cur=mysql.cursor()
-        cur.execute("INSERT INTO Myuser(firstname, lastname) VALUES (%s, %s)", (firstName, lastName))
+        cur1=mysql.cursor()
+        cur1.execute("INSERT INTO Userdetail(firstname, lastname) VALUES (%s, %s)", (firstName, lastName))
         mysql.commit()
-        cur.close()
+        cur1.close()
         return 'success'
     return render_template('index.html')
 
